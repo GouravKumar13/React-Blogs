739465c42e27c2d6fbcb3be5ee27670a2345bb09
GouravKumar13 <gouravkumarat@gmail.com>
2023-10-27 12:17:24 +0530

    .

--- src/Pages/EditPost.jsx
+++ src/Pages/EditPost.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import PostForm from "../Components/PostForm/PostFrom"
 import { useNavigate, useParams } from 'react-router-dom'
 import appwriteBlogService from '../appwrite/BlogsOperations'
+
 const EditPost = () => {
     const [post, setPost] = useState(null)
     const { slug } = useParams()
