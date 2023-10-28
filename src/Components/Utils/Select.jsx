a043d3e27929c439c0ec6c4f8ee30d46197f5291
GouravKumar13 <gouravkumarat@gmail.com>
2023-10-29 02:34:18 +0530

    fix

--- src/Components/Utils/Select.jsx
+++ src/Components/Utils/Select.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useId } from 'react'
 
-const Select = ({ label, options = [], className = "" }, ref) => {
+const Select = ({ label, options = [], className = "", props }, ref) => {
     const id = useId()
     return (
         <select className={ ` p-1 ${className}` } id={ id } ref={ ref } { ...props }>
